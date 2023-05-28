39dc2e180b1016673db4fb6f102f6cf93fdf301b
FelipeMunhoz18 <f.munoz7@duocuc.cl>
2023-05-27 21:21:17 -0400

    Gatito.
    ──────▄▀▄─────▄▀▄
    ─────▄█░░▀▀▀▀▀░░█▄
    ─▄▄──█░░░░░░░░░░░█──▄▄
    █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█

--- MusicPro4_PhantomPain/core/static/core/JS/script.js
+++ MusicPro4_PhantomPain/core/static/core/JS/script.js
@@ -26,7 +26,33 @@ function myFunction() {
       })
   })
 
-
+  const imgs = document.querySelectorAll('.img-select a');
+  const imgBtns = [...imgs];
+  let imgId = 1;
+  
+  imgBtns.forEach((imgItem) => {
+      imgItem.addEventListener('click', (event) => {
+          event.preventDefault();
+          imgId = imgItem.dataset.id;
+          slideImage();
+      });
+  });
+  
+  function slideImage(){
+      const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+  
+      document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+  }
+  
+  window.addEventListener('resize', slideImage);
+  function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
 
 
 
--- MusicPro4_PhantomPain/core/views.py
+++ MusicPro4_PhantomPain/core/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/index.html')
 # Create your views here.
+def producto(request):
+    return render(request, 'core/producto.html')
