beda7d673e57f46759a0eeeddecd412922bba738
Afix42 <101334091+Afix42@users.noreply.github.com>
2023-05-29 12:30:06 -0400

    Vamos como avión.
    ──────────▄
    ────────▄██
    ─▄▀██▀█▀█▀███▀
    ▀▀▀▀▀████▀▀▀
    ──────▀██

--- MusicPro4_PhantomPain/core/urls.py
+++ MusicPro4_PhantomPain/core/urls.py
@@ -6,10 +6,10 @@ from .views import home,producto,html_registro,html_login,tienda,vista_usuario,v
 urlpatterns = [
     path('', home, name='index'),
     path('producto', producto, name='producto'),
-    path('Registro', html_registro, name='Registro'),
-    path('Login', html_login, name='Login'),
+    path('registro', html_registro, name='Registro'),
+    path('login', html_login, name='Login'),
     path('tienda', tienda, name='tienda'),
-    path('Usuario', vista_usuario, name='vista_usuario'),
+    path('usuario', vista_usuario, name='vista_usuario'),
     path('admin', vista_admin, name='vista_admin'),
     path('tienda_admin', tienda_admin, name='tienda_admin'),
     path('agregar_productos', agregar_productos, name='agregar_productos'),
